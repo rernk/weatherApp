0a797fe9d546b381cadb51880b1db756e5e3bea2
mjuracka <mjuracka@microstep-hdo.sk>
2021-01-23 23:40:32 +0100

    WeatherUP vs.2.0

---
+++ src/pages/weather/weather.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {WeatherProvider} from '../../providers/weather/weather';
+
+/**
+ * Generated class for the WeatherPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-weather',
+  templateUrl: 'weather.html',
+})
+export class WeatherPage {
+  weather:any;
+  weather_part:string;
+  weather_icon1:string;
+  weather_icon2:string;
+  location:{
+    city:string,
+    state:string
+  }
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private weatherProvider:WeatherProvider) {
+  }
+
+  ionViewWillEnter()
+  {
+    this.location =
+    {
+      //city: 'Bratislava',
+      //city: 'Nové Mesto nad Váhom',
+      city: 'Miami',
+      state: 'USA'
+      //state:'sk'
+    }
+
+    this.weatherProvider.getWeather(this.location.city,this.location.state).subscribe(weather => {
+      //Concole output
+      //console.log(weather);
+      this.weather_part = weather.weather[0].icon
+      this.weather_icon1 = "https://openweathermap.org/img/wn/"
+      this.weather_icon2 = "@2x.png";
+      this.weather = weather;
+    });
+  }
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad WeatherPage');
+  }
+
+}
---
+++ src/providers/weather/weather.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class WeatherProvider {
+  api_key= 'fbbb2ea3548a7c9c9e06c94a09507656';
+  url_part_one;
+  url_part_two ='&units=metric&appid='+this.api_key;
+
+  constructor(public http: HttpClient) {
+    console.log('Hello WeatherProvider Provider');
+    //https://api.openweathermap.org/data/2.5/weather?q=Bratislava,sk&units=metric&APPID=fbbb2ea3548a7c9c9e06c94a09507656
+        this.url_part_one ='https://api.openweathermap.org/data/2.5/weather?q=';
+  }
+  getWeather(city,state)
+  {
+   return this.http.get(this.url_part_one+city+','+state+this.url_part_two);
+  }
+
+}
